35702ac14f4eabef4f6f53119d78146be38323cc
Aleksandra Rosinska <aleks.rosinska@gmail.com>
2020-05-11 16:40:37 +0200

    Clean repo

--- kodilla-intro/src/main/java/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Aleksandra");
-        System.out.println(29);
-        System.out.println('R');
-    }
-}

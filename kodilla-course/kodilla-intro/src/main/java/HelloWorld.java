8a497328f7a8dca2704ef6fdb20e4e601647541a
Aleksandra Rosinska <aleks.rosinska@gmail.com>
2020-05-12 13:20:45 +0200

    Zmiany do naprawienia mastera

---
+++ kodilla-course/kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Aleksandra");
+        System.out.println(29);
+        System.out.println('R');
+    }
+}
---
+++ kodilla-course/kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String name = "Aleksanda";
+        int age = 29;
+        double height = 1.68;
+        char drivingLicenceCat = 'B';
+        boolean isGoodStudent = true;
+        String job = "done";
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(drivingLicenceCat);
+        System.out.println(isGoodStudent);
+        System.out.println(job);
+    }
+}
